cae7e3dcb0f9eec8b21d53ad25e118d228079478
Osman Kartal <okartal3434@gmail.com>
2020-06-28 13:50:54 +0300

    create package

---
+++ generate.py
@@ -0,0 +1,10 @@
+from utils import sample_latent_point
+from keras.models import load_model
+import matplotlib.pyplot as plt
+
+model = load_model('gan.h5')
+
+def generate_image():
+	fake_image = model.predict(sample_latent_point(1, 256))
+	plt.imshow(fake_image[0, :, :, 0], cmap="gray")
+	plt.show()
